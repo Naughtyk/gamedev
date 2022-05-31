0f2a8916636740fa9ab39eb59136df67a797f628
Naughtyk <akazak96@mail.ru>
2022-05-31 18:20:42 +0300

    Making a weapon animations and graphics

--- level.py
+++ level.py
@@ -1,9 +1,15 @@
 #import groups as groups
+#import boundary as boundary
 import pygame
 from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
+from weapon import Weapon
+
+import random
+from random import choice
 
 
 class Level:
@@ -17,14 +23,45 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                elif col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+
+        layout = {
+            'boundary': import_csv_layout('map/map_FloorBlocks.csv'),
+            'grass': import_csv_layout('map/map_Grass.csv'),
+            'object': import_csv_layout('map/map_Objects.csv')
+        }
+
+        graphics = {
+            'grass': import_folder('graphics/grass'),
+            'objects': import_folder('graphics/objects')
+        }
+
+        for style, layout in layout.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x, y), [self.obstacle_sprites], 'invisible')
+                        if style == 'grass':
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'grass', random_grass_image)
+                        if style == 'object':
+                            surf = graphics['objects'][int(col)]
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
+
+
+
+
+                        # if col == 'x':
+                        #     Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                        # elif col == 'p':
+                        #     self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+
+        self.player = Player((2000, 1400), [self.visible_sprites], self.obstacle_sprites, self.create_attack)
+
+    def create_attack(self):
+        Weapon(self.player, [self.visible_sprites])
 
     def run(self):
         # update and draw the game
@@ -44,16 +81,17 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.offset = pygame.math.Vector2(-100, -200)
 
         # creating the floor
-        self.floor_surface = pygame.image.load('5 - level graphics/graphics/tilemap/ground.png').convert()
-        self.floor_rect = self.floor_surface.get_rect(topleft=(0, 0))
+        self.floor_surf = pygame.image.load('graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
 
     def custom_draw(self, player):
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        offset_pos = self.floor_rect.topleft - self.offset
-        self.display_surface.blit(self.floor_surface, offset_pos)
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         for sprite in self.sprites():
         #for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
