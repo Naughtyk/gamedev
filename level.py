b1ad4da13573115a12af450a538a6c76772c1416
Naughtyk <akazak96@mail.ru>
2022-06-07 22:31:49 +0300

    Making a weapon animations and graphics

--- level.py
+++ level.py
@@ -22,6 +22,9 @@ class Level:
         self.obstacle_sprites = pygame.sprite.Group()
         self.create_map()
 
+        # attack sprites
+        self.current_attack = None
+
     def create_map(self):
 
         layout = {
@@ -58,10 +61,16 @@ class Level:
                         # elif col == 'p':
                         #     self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
-        self.player = Player((2000, 1400), [self.visible_sprites], self.obstacle_sprites, self.create_attack)
+        self.player = Player((2000, 1400), [self.visible_sprites], self.obstacle_sprites,
+                             self.create_attack, self.destroy_attack)
 
     def create_attack(self):
-        Weapon(self.player, [self.visible_sprites])
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
+
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
 
     def run(self):
         # update and draw the game
